64a96dd7b78c037670bb6c2ea878ecaa23f850a2
Achilleas Kotsis <achille@kotsis.net>
2024-10-15 13:10:31 +0300

    unipi: Binary sensor use pin string format

--- esphome/components/unipi/binary_sensor/unipi_binary_sensor.cpp
+++ esphome/components/unipi/binary_sensor/unipi_binary_sensor.cpp
@@ -1,26 +1,53 @@
 #include "unipi_binary_sensor.h"
 #include "esphome/core/log.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include <cstdio>
 
 namespace esphome {
 namespace unipi {
 
 static const char *const TAG = "unipi.binary_sensor";
+static const char *const UNIPI_PLC = "/run/unipi-plc/by-sys";
 
 void UnipiBinarySensor::setup() {
-  this->pin_->setup();
-  this->publish_initial_state(this->pin_->digital_read());
+  std::string filename;
+
+  filename = str_sprintf("%s/%s/value", UNIPI_PLC, this->pin_.c_str());
+
+  ESP_LOGCONFIG(TAG, "Setting up Unipi, pin %s...", this->pin_.c_str());
+  ESP_LOGD(TAG, "Opening... %s", filename.c_str());
+  this->fp_ = fopen(filename.c_str(), "r");
+  if (!this->fp_) {
+    ESP_LOGE(TAG, "Could not open %s: %s", filename.c_str(), strerror(errno));
+    this->mark_failed();
+    return;
+  }
+  setbuf(this->fp_, nullptr);
+
+  //  int val = getc(this->fp_);
+  //  fflush(this->fp_);
+  //  this->publish_initial_state(val != '0');
 }
 
 void UnipiBinarySensor::dump_config() {
   LOG_BINARY_SENSOR("", "Unipi Binary Sensor", this);
-  LOG_PIN("  Pin: ", this->pin_);
+
+  if (!this->is_failed()) {
+    ESP_LOGCONFIG(TAG, "  Pin: %s", this->pin_.c_str());
+  } else {
+    ESP_LOGE(TAG, "  Pin: %s (inoperable)", this->pin_.c_str());
+  }
 }
 
 void UnipiBinarySensor::update() {
-  this->publish_state(this->pin_->digital_read());
+  rewind(this->fp_);
+  int val = getc(this->fp_);
+  fflush(this->fp_);
+
+  this->publish_state(val != '0');
 }
 
-float UnipiBinarySensor::get_setup_priority() const { return setup_priority::HARDWARE; }
+float UnipiBinarySensor::get_setup_priority() const { return setup_priority::IO; }
 
 }  // namespace unipi
 }  // namespace esphome
