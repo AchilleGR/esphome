da2749d8ad99150946e1c353dac8d6518f2ada94
Achilleas Kotsis <achille@kotsis.net>
2024-10-15 13:10:31 +0300

    unipi: Binary sensor use pin string format

--- esphome/components/unipi/binary_sensor/unipi_binary_sensor.h
+++ esphome/components/unipi/binary_sensor/unipi_binary_sensor.h
@@ -3,21 +3,25 @@
 #include "../unipi.h"
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
+#include "esphome/core/helpers.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
+#include <cstdio>
 
 namespace esphome {
 namespace unipi {
 
 class UnipiBinarySensor : public binary_sensor::BinarySensor, public PollingComponent {
  public:
-  void set_pin(UnipiGPIOPin *pin) { this->pin_ = pin; }
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override;
   void update() override;
 
+  void set_pin(std::string pin) { this->pin_ = pin; }
+
  protected:
-  UnipiGPIOPin *pin_;
+  std::string pin_;
+  FILE *fp_;
 };
 
 }  // namespace unipi
